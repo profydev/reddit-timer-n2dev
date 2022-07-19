be8c0afe8b010db0aa607d3b63e7f28543ab349e
n2 <38994662+n2dev@users.noreply.github.com>
2022-07-20 01:27:02 +0900

    Add app skeleton #1 (#1)
    * Add app skeleton (#1)
    
    * Fix app skeleton (#1)
    
    * Fix app skeleton (#1)
    
    * Fix app skeleton? (#1)
    
    * Change app title and meta description #1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <h1>Reddit Timer</h1>
+        <Normalize />
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" />
+          <Route path="/search" />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
---
+++ src/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: ${(props) => props.theme.font.family.default};
+    font-size: ${(props) => props.theme.font.size.default};
+    line-height: ${(props) => props.theme.font.lineHeight.default};
+    color: ${(props) => props.theme.color.text};
+  }
+`;
+
+export default GlobalStyle;
