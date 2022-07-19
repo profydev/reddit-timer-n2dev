9cffe6c70f3ba0dd365f1e0d4c38aa04e838dfe8
hellonagi <nagisa.n102@gmail.com>
2022-07-19 16:47:49 +0900

    Fix app skeleton (#1)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Search from './components/Search';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './GlobalStyle';
+import theme from './theme';
 
 function App() {
   return (
-    <BrowserRouter>
-      <h1>Reddit Timer</h1>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <h1>Reddit Timer</h1>
+        <Normalize />
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" />
+          <Route path="/search" />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'normalize.css';
-import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
